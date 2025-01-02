172c1c5c2c41d2865ebed68cc0992eac42b95b55
“AnzhelikaBereza” <anzhelika.haf@gmail.com>
2025-01-02 17:51:33 +0200

    commit Test classes

---
+++ src/test/java/selenium/SeleniumTestClass.java
@@ -0,0 +1,19 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class SeleniumTestClass {
+    @Test
+    public void testClickButton() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-gpu");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://stackoverflow.com/");
+    }
+}
